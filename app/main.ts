6b5c92dd6f112d2a36d4577e5ea2a03217ba1430
Rodrigo Pina Lozano (CI/BDM-EU) <rodrigo.pina@de.bosch.com>
2017-10-23 21:06:58 +0200

    tagged, readme

--- app/main.ts
+++ app/main.ts
@@ -21,7 +21,7 @@ firebase.init({
   }
 }).then(
   function (instance) {
-    console.log("firebase.init done!");
+    console.log("firebase.init done");
   },
   function (error) {
     console.log("firebase.init error: " + error);
